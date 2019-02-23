33eb069e618f2465b2770792699f2782eb111710
Sara Ghaemi <ghaemi.sr@gmail.com>
2019-02-23 12:13:46 -0700

    REFACTOR: changed the names of the variable and added some comments

--- gomoku/scoreboard.py
+++ gomoku/scoreboard.py
@@ -1,16 +1,20 @@
 class Scoreboard:
 
     def __init__(self):
-        self.result = {'black': 0, 'white': 0, 'tie': 0}
+        self.current_scoreboard = {'black': 0, 'white': 0, 'tie': 0}
 
     def set_new_result(self, winner):
+        """
+        :param winner: A string showing the winner
+        :return: Changed scoreboard
+        """
         if winner == 'black':
-            self.result['black'] += 1
+            self.current_scoreboard['black'] += 1
         elif winner == 'white':
-            self.result['white'] += 1
+            self.current_scoreboard['white'] += 1
         elif winner == 'tie':
-            self.result['tie'] += 1
+            self.current_scoreboard['tie'] += 1
         else:
             raise ValueError('Could not understand ' + winner + ' as a winner')
 
-        return self.result
+        return self.current_scoreboard
