a41dc9fb0eaeb94964b3a45cd30d019caa1e51a5
Changyuan Lin <linchangyuan1996@gmail.com>
2019-02-24 15:03:09 -0700

    GREEN: Added implementation of move method in Chessboard class

--- gomoku/chessboard.py
+++ gomoku/chessboard.py
@@ -1,5 +1,17 @@
 class Chessboard:
 
+    class IllegalCoordinateException(Exception):
+        def __init__(self):
+            Exception.__init__(self)
+
+    class IllegalPlayerException(Exception):
+        def __init__(self):
+            Exception.__init__(self)
+
+    class IllegalMoveException(Exception):
+        def __init__(self):
+            Exception.__init__(self)
+
     def __init__(self):
         self.current_player = "None"
         self.chessboard_matrix = []
@@ -25,4 +37,15 @@ class Chessboard:
         return self.current_player
 
     def move(self, x, y, player):
-        pass
+        if type(x) != int or type(y) != int:
+            raise self.IllegalCoordinateException
+        if x < 0 or x > 18 or y < 0 or y > 18:
+            raise self.IllegalCoordinateException
+        if not (player == "White" or player == "Black"):
+            raise self.IllegalPlayerException
+        if self.chessboard_matrix[x][y] != 0:
+            raise self.IllegalMoveException
+        if player == "Black":
+            self.chessboard_matrix[x][y] = 1
+        elif player == "White":
+            self.chessboard_matrix[x][y] = 2
