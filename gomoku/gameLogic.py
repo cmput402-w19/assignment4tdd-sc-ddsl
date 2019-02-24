d5c3bb3d5e030ddf9cbaec916279fddb74a9ecce
Changyuan Lin <linchangyuan1996@gmail.com>
2019-02-24 15:54:44 -0700

    GREEN: Added implementation of check_horizontal method in GameLogic class

--- gomoku/gameLogic.py
+++ gomoku/gameLogic.py
@@ -3,5 +3,29 @@ class GameLogic:
     def __init__(self):
         self.winner = "None"
 
-    def check_horizontal(self, chessboard_matrix, last_move_row, last_move_col):
-        return False
+    def check_horizontal(self, chessboardMatrix, lastMoveRow, lastMoveCol):
+        leftCount = 0
+        rightCount = 0
+        stone = chessboardMatrix[lastMoveRow][lastMoveCol]
+        if stone == 0:
+            return False
+        for i in range(1, 5):
+            if lastMoveCol - i >= 0:
+                if chessboardMatrix[lastMoveRow][lastMoveCol - i] == stone:
+                    leftCount = leftCount + 1
+                else:
+                    break
+            else:
+                break
+        for i in range(1, 5):
+            if lastMoveCol + i <= 18:
+                if chessboardMatrix[lastMoveRow][lastMoveCol + i] == stone:
+                    rightCount = rightCount + 1
+                else:
+                    break
+            else:
+                break
+        if leftCount + rightCount + 1 >= 5:
+            return True
+        else:
+            return False
