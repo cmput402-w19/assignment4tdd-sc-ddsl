012c92693573343ead1069c5ea098a220f623e5b
Changyuan Lin <linchangyuan1996@gmail.com>
2019-02-24 15:36:59 -0700

    RED: Wrote tests for check_horizontal method of GameLogic class
    Test cases and skeleton for check_horizontal method of GameLogic class are added.
    Modifed .travis.yml to test GameLogic class

---
+++ gomoku/gameLogic.py
@@ -0,0 +1,7 @@
+class GameLogic:
+
+    def __init__(self):
+        self.winner = "None"
+
+    def check_horizontal(self, chessboard_matrix, last_move_row, last_move_col):
+        return False
---
+++ test/gameLogicTest.py
@@ -0,0 +1,39 @@
+import unittest
+from gomoku.gameLogic import GameLogic
+
+
+class TestGameLogic(unittest.TestCase):
+    def setUp(self):
+        self.gl = GameLogic()
+
+    def test_check_horizontal(self):
+        chessboard_matrix = [
+            [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]]
+        self.assertEqual(True, self.gl.check_horizontal(chessboard_matrix, 0, 0))
+        self.assertEqual(True, self.gl.check_horizontal(chessboard_matrix, 0, 1))
+        self.assertEqual(True, self.gl.check_horizontal(chessboard_matrix, 0, 4))
+        self.assertEqual(True, self.gl.check_horizontal(chessboard_matrix, 18, 14))
+        self.assertEqual(True, self.gl.check_horizontal(chessboard_matrix, 18, 15))
+        self.assertEqual(True, self.gl.check_horizontal(chessboard_matrix, 18, 18))
+
+
+if __name__ == '__main__':
+    unittest.main()
