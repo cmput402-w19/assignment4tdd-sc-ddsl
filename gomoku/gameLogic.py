06917512b17e0931c1063cd747804f3a290f0b44
Changyuan Lin <linchangyuan1996@gmail.com>
2019-02-24 16:46:08 -0700

    GREEN: Added implementation of check_vertical method in GameLogic class

--- gomoku/gameLogic.py
+++ gomoku/gameLogic.py
@@ -37,4 +37,34 @@ class GameLogic:
             return False
 
     def check_vertical(self, chessboard_matrix, last_move_row, last_move_col):
-        return False
+        """
+
+        Check five-in-a-row vertically
+        :param chessboard_matrix: Current chessboard matrix, last_move_row: Row number of the last move, last_move_col: Column number of the last move
+        :return: True or False
+        """
+        up_count = 0
+        down_count = 0
+        stone = chessboard_matrix[last_move_row][last_move_col]
+        if stone == 0:
+            return False
+        for i in range(1, 5):
+            if last_move_row - i >= 0:
+                if chessboard_matrix[last_move_row - i][last_move_col] == stone:
+                    up_count = up_count + 1
+                else:
+                    break
+            else:
+                break
+        for i in range(1, 5):
+            if last_move_row + i <= 18:
+                if chessboard_matrix[last_move_row + i][last_move_col] == stone:
+                    down_count = down_count + 1
+                else:
+                    break
+            else:
+                break
+        if up_count + down_count + 1 >= 5:
+            return True
+        else:
+            return False
