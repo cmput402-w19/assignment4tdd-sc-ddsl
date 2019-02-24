d8b188db7aea68ffa81430c4d1d5251d27ca0c06
Changyuan Lin <linchangyuan1996@gmail.com>
2019-02-24 16:08:44 -0700

    changed the names of the variable and added some comments
    Change the variable names to follow Python naming conventions.
    Add comments about check_horizontal method

--- gomoku/gameLogic.py
+++ gomoku/gameLogic.py
@@ -3,29 +3,35 @@ class GameLogic:
     def __init__(self):
         self.winner = "None"
 
-    def check_horizontal(self, chessboardMatrix, lastMoveRow, lastMoveCol):
-        leftCount = 0
-        rightCount = 0
-        stone = chessboardMatrix[lastMoveRow][lastMoveCol]
+    def check_horizontal(self, chessboard_matrix, last_move_row, last_move_col):
+        """
+
+        Check five-in-a-row horizontally
+        :param chessboard_matrix: Current chessboard matrix, last_move_row: Row number of the last move, last_move_col: Column number of the last move
+        :return: True or False
+        """
+        left_count = 0
+        right_count = 0
+        stone = chessboard_matrix[last_move_row][last_move_col]
         if stone == 0:
             return False
         for i in range(1, 5):
-            if lastMoveCol - i >= 0:
-                if chessboardMatrix[lastMoveRow][lastMoveCol - i] == stone:
-                    leftCount = leftCount + 1
+            if last_move_col - i >= 0:
+                if chessboard_matrix[last_move_row][last_move_col - i] == stone:
+                    left_count = left_count + 1
                 else:
                     break
             else:
                 break
         for i in range(1, 5):
-            if lastMoveCol + i <= 18:
-                if chessboardMatrix[lastMoveRow][lastMoveCol + i] == stone:
-                    rightCount = rightCount + 1
+            if last_move_col + i <= 18:
+                if chessboard_matrix[last_move_row][last_move_col + i] == stone:
+                    right_count = right_count + 1
                 else:
                     break
             else:
                 break
-        if leftCount + rightCount + 1 >= 5:
+        if left_count + right_count + 1 >= 5:
             return True
         else:
             return False
