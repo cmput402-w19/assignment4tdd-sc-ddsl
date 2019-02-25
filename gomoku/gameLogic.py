efdc480cdfe7594f627fe3e4838273190418f906
Changyuan Lin <linchangyuan1996@gmail.com>
2019-02-24 17:53:28 -0700

    GREEN: Added implementation of check_lower_diagonal method in GameLogic class

--- gomoku/gameLogic.py
+++ gomoku/gameLogic.py
@@ -69,5 +69,29 @@ class GameLogic:
         else:
             return False
 
-    def check_lower_diagonal(self, chessboardMatrix, lastMoveRow, lastMoveCol):
-        return False
+    def check_lower_diagonal(self, chessboard_matrix, last_move_row, last_move_col):
+        left_count = 0
+        right_count = 0
+        stone = chessboard_matrix[last_move_row][last_move_col]
+        if stone == 0:
+            return False
+        for i in range(1, 5):
+            if last_move_row + i <= 18 and last_move_col + i >= 0:
+                if chessboard_matrix[last_move_row + i][last_move_col - i] == stone:
+                    left_count = left_count + 1
+                else:
+                    break
+            else:
+                break
+        for i in range(1, 5):
+            if last_move_row - i >= 0 and last_move_col + i <= 18:
+                if chessboard_matrix[last_move_row - i][last_move_col + i] == stone:
+                    right_count = right_count + 1
+                else:
+                    break
+            else:
+                break
+        if left_count + right_count + 1 >= 5:
+            return True
+        else:
+            return False
